9a1ec3894df32246627f585733ac03b7c933e0d5
kephrim <kephrim@163.com>
2020-11-20 14:15:31 +0800

    第三次提交server

---
+++ server/apps/admin/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AdminModule } from './admin.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AdminModule);
+  await app.listen(3000);
+  console.log("服务启动成功");
+  
+}
+bootstrap();
